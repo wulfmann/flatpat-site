78975bada85847414af5a1600575bf692315a87f
Joseph Snell <joepsnell@gmail.com>
2020-06-27 08:43:11 -0500

    update

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,12 @@ import styles from '../styles/components/Header.module.scss';
 export default function() {
   return (
     <div className={styles.nav}>
-      <div className={styles.title}>
-        FLAT PAT
-      </div>
+      <Link href="/">
+        <a>
+          <div className={styles.title}>FLAT PAT</div>
+        </a>
+      </Link>
+
       <div className={styles.links}>
         <Link href="/how"><a>how</a></Link>
       </div>
