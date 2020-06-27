564fe473fe7b0effbacfe39f0475e39990f20bcf
Joseph Snell <joepsnell@gmail.com>
2020-06-27 12:22:42 -0500

    update styles

---
+++ pages/submit.js
@@ -0,0 +1,11 @@
+import Layout from '../layouts/default';
+
+export default function() {
+  return (
+    <Layout title="Submit">
+      <section>
+        Submit
+      </section>
+    </Layout>
+  )
+}
