3e91a9318c1f007c40ca1e4650e5072ab6690746
Joseph Snell <joepsnell@gmail.com>
2020-06-26 19:41:51 -0500

    add link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function() {
   return (
     <div>
       <h1>Im Flat Pat</h1>
+      <Link href=`/about`>   
+        <a>About</a>
+      </Link>
     </div>
   )
 }
