647eb05ad6d71ec8ada7856bc4cec74c18954255
Joseph Snell <joepsnell@gmail.com>
2020-06-27 08:51:05 -0500

    update

--- pages/how.js
+++ pages/how.js
@@ -3,7 +3,9 @@ import Layout from '../layouts/default';
 export default function() {
   return (
     <Layout>
-      <h2>Ground Rules</h2>
+      <section>
+        <h2>Ground Rules</h2>
+      </section>
     </Layout>
   )
 }
