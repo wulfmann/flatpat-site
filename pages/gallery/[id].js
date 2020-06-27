09209c572f7385fd55426e8fb935cabdf0691233
Joseph Snell <joepsnell@gmail.com>
2020-06-27 16:18:08 -0500

    update

--- pages/gallery/[id].js
+++ pages/gallery/[id].js
@@ -15,7 +15,7 @@ export default function() {
   return (
     <Layout title={data.title}>
       <section>
-        <div className="placeholder"></div>
+        <div className={styles.placeholder}></div>
         <div className={styles.caption}>{data.caption}</div>
          <div className={styles.name}>{data.name}</div>
       </section>
