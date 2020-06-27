5c7b82b1f3b8d331142a65d55221bd3e6358f1c3
Joseph Snell <joepsnell@gmail.com>
2020-06-27 11:45:07 -0500

    ipdate layout

--- layouts/default.js
+++ layouts/default.js
@@ -1,10 +1,11 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export default function({ children }) {
+export default function({ children, title }) {
   return (
     <main>
       <Header />
+      <h1>{title}</h1>
       <div>{children}</div>
       <Footer />
     </main>
--- pages/how.js
+++ pages/how.js
@@ -2,9 +2,18 @@ import Layout from '../layouts/default';
 
 export default function() {
   return (
-    <Layout>
+    <Layout title="How">
       <section>
-        <h2>Ground Rules</h2>
+        <h2>Assemble</h2>
+        <p>Assemble your flatpat.</p>
+      </section>
+      <section>
+        <h2>Photograph</h2>
+        <p>Photograph your flatpat</p>
+      </section>
+      <section>
+        <h2>Submit</h2>
+        <p>Submit your flatpat</p>
       </section>
     </Layout>
   )
