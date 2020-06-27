71a6eacf0c5a635bbbee4f90a5219ed7228fdc00
Joseph Snell <joepsnell@gmail.com>
2020-06-26 22:03:51 -0500

    add font

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import App from 'next/app';
 import '../styles/global.scss';
 
+if (typeof window !== 'undefined') {
+  const WebFont = require('webfontloader');
+  WebFont.load({
+    google: {
+      families: ['Krona One']
+    }
+  });
+}
+
 export default function({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
