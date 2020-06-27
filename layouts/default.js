22ea6ca95f95589d034310b445a0eb44d0b0e0ec
Joseph Snell <joepsnell@gmail.com>
2020-06-27 11:59:31 -0500

    add layout styles

--- layouts/default.js
+++ layouts/default.js
@@ -1,11 +1,14 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import styles from '../styles/layouts/default.module.scss';
 
 export default function({ children, title }) {
   return (
     <main>
       <Header />
-      <h1>{title}</h1>
+      <div className={styles.title}>
+        <h1>{title}</h1>
+      </div>
       <div>{children}</div>
       <Footer />
     </main>
