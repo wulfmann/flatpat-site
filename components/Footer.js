b3521e8793f0826c27ff20673e860d7603ad886f
Joseph Snell <joepsnell@gmail.com>
2020-06-27 10:55:33 -0500

    add text to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ import styles from '../styles/components/Footer.module.scss';
 export default function() {
   return (
     <div className={styles.footer}>
+      <h2>FLAT PAT</h2>
     </div>
   )
 }
