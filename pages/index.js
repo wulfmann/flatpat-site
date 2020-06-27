4c4bc2c89b7dd0739dd8bc2605e904b643ba7cde
Joseph Snell <joepsnell@gmail.com>
2020-06-26 21:42:30 -0500

    add logo to home page

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,9 @@ import Link from 'next/link';
 export default function() {
   return (
     <div>
+       <img src="/logo.svg" alt="Flat Pat" width="150"/>
       <h1>Im Flat Pat</h1>
       <p>a pat that is flat</p>
-      <Link href="/about">   
-        <a>About</a>
-      </Link>
     </div>
   )
 }
