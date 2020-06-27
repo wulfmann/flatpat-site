a264f32729c5183f35ce36ffffa11e2bbae1bb7a
Joseph Snell <joepsnell@gmail.com>
2020-06-27 10:53:39 -0500

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+import styles from '../styles/components/Footer.module.scss';
+
+export default function() {
+  return (
+    <div className={styles.footer}>
+    </div>
+  )
+}
--- layouts/default.js
+++ layouts/default.js
@@ -1,10 +1,12 @@
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function({ children }) {
   return (
     <main>
       <Header />
       <div>{children}</div>
+      <Footer />
     </main>
   )
 }
