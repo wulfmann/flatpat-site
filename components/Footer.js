43b839811ed05a1f237c00bc697d5375c6475a18
Joseph Snell <joepsnell@gmail.com>
2020-06-27 16:04:15 -0500

    add footer li

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,20 @@ export default function() {
   return (
     <div className={styles.footer}>
       <h2>FLAT PAT</h2>
+      <div className={styles.links}>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+                <Link href="/guide">
+          <a>Guide</a>
+        </Link>
+                <Link href="/gallery">
+          <a>Gallery</a>
+        </Link>
+                <Link href="/submit">
+          <a>Submit</a>
+        </Link>
+      </div>
     </div>
   )
 }
