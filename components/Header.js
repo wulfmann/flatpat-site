792023ab45950f1ef347746a6c063cf7f986a5ce
Joseph Snell <joepsnell@gmail.com>
2020-06-27 17:31:41 -0500

    revert

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,6 @@ import styles from '../styles/components/Header.module.scss';
 export default function() {
   return (
     <div className={styles.nav}>
-      <div className={styles.logo}>
-        <img src="/minimal.svg" width="60" alt="logo" />
-      </div>
       <Link href="/">
         <a>
           <div className={styles.title}>FLAT PAT</div>
