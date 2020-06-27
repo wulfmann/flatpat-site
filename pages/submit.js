0639b89991acf832413b57b239339af81295f064
Joseph Snell <joepsnell@gmail.com>
2020-06-27 12:28:50 -0500

    add form

--- pages/submit.js
+++ pages/submit.js
@@ -1,10 +1,24 @@
 import Layout from '../layouts/default';
 
 export default function() {
+  function onSubmit(e) {
+    e.preventDefault();
+    alert('Thank you for your submission');
+  }
+
   return (
     <Layout title="Submit">
       <section>
-        Submit
+        <form onSumbit={onSubmit}>
+          <label for="caption">Caption</label>
+          <input type="text" name="caption"/>
+          
+          <label for="name">Name</label>
+          <input type="text" name="name"/>
+          
+          <label for="file">Image</label>
+          <input type="file" name="file"/>
+        </form>
       </section>
     </Layout>
   )
