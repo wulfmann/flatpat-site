f240fc4a49eb767eb39e10231ebc85d4b16224fb
Joseph Snell <joepsnell@gmail.com>
2020-06-26 22:07:19 -0500

    center

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function() {
   return (
-    <div>
+    <div className="center">
        <img src="/logo.svg" alt="Flat Pat" width="150"/>
       <h1>Im Flat Pat</h1>
       <p>a pat that is flat</p>
