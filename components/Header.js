f21eeac1d92785aaf8122e1d6415eb4be9cdf3bf
Joseph Snell <joepsnell@gmail.com>
2020-06-28 08:18:28 -0500

    add menu

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import styles from '../styles/components/Header.module.scss';
 
 export default function() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className={styles.nav}>
       <Link href="/">
@@ -9,10 +11,13 @@ export default function() {
           <div className={styles.title}>FLAT PAT</div>
         </a>
       </Link>
-
-      <div className={styles.links}>
-        <Link href="/guide"><a>GUIDE</a></Link>
-      </div>
+      <div className={styles.menu} onClick={e=>setMenuOpen(!menu)}>MENU</div>
+      
+      {menuOpen && (
+        <div>
+          menu
+        </div>
+      )}
     </div>
   )
 }
