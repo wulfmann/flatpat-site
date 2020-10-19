b34a7f3afc33562db88efed44cb07152b49dde3e
Joe Snell <joe.snell@daveramsey.com>
2020-10-19 07:14:40 -0500

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,30 @@
-import Link from 'next/link';
-import Layout from '../layouts/default';
+import styles from '../styles/pages/index.module.scss'
 
-export default function() {
+export default function () {
   return (
-    <Layout title="Flat Pat">
+    <div className={styles.container}>
+      <article className={styles.text}>
+        <p>Flat Pat is a social experience. A twist on <a href="https://en.wikipedia.org/wiki/Flat_Stanley">Flat Stanley</a>, Flat Pat aims to travel to as many different places as possible.</p>
+        
+        <p>Flat Pat is a secret from his likeness.</p>
+        
+        <p>We are documenting Flat Pat’s life throughout the year, and will be presenting this project as a surprise at the end of the year.</p>
+        
+        <p>Flat Pat comes in many forms in very limited quantities. To get flat pat, email: <a href="mailto:get@imflatpat.com">get@imflatpat.com</a></p>
+        
+        <p>There are only two steps to participate:</p>
+        
+        <ul>
+          <li>1. Get Flat Pat </li>
+          <li>2. Document his life with you</li>
+        </ul>
+        
+        <p>We will post your submissions on social media, and have the chance to be selected for the Flat Pat book that will be printed at the end of the year.</p>
+      </article>
+
       <section>
-       <img src="/logo.svg" alt="Flat Pat" width="150"/>
+        <img src="/logo.svg" alt="Flat Pat" width="100" />
       </section>
-      <section>
-        <h2>Gallery</h2>
-        <div className="grid home-grid">
-          <Link href="/gallery/1">
-            <div className="placeholder"></div>
-          </Link>
-          <Link href="/gallery/1">
-            <div className="placeholder"></div>
-          </Link>
-          <Link href="/gallery/1">
-            <div className="placeholder"></div>
-          </Link>
-          <Link href="/gallery/1">
-            <div className="placeholder"></div>
-          </Link>
-        </div>
-        <div className="home-link">
-         <Link href="/gallery">
-            <a>View More</a>
-         </Link>
-        </div>
-      </section>
-      <section>
-        <h2>Submit</h2>
-      </section>
-    </Layout>
-  )
+    </div>
+  );
 }
